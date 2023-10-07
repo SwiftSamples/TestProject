e9c139f4d56c0adabe484b32ecc1ad574aebda3d
Swift Samples <samplefortest1234@gmail.com>
2023-10-07 19:05:53 +0530

    branch2 commit

--- TestProject/ViewController4.swift
+++ TestProject/ViewController4.swift
@@ -48,8 +48,6 @@ class ViewController4: UIViewController, CLLocationManagerDelegate, UITextFieldD
         let curLatitude = location?.coordinate.latitude
         let curLongitude = location?.coordinate.longitude
         
-        print("current latitude \(curLatitude), longitude: \(curLongitude)")
-
         let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 6.0)
         self.gMapView?.animate(to: camera)
         //Finally stop updating location otherwise it will come again and again in this delegate
@@ -79,85 +77,11 @@ class ViewController4: UIViewController, CLLocationManagerDelegate, UITextFieldD
         previousMarker = marker
         marker.map = gMapView
     }
-    
-//    func getPolylineRoute(from source: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D){
-//
-//            let config = URLSessionConfiguration.default
-//            let session = URLSession(configuration: config)
-//
-//            let url = URL(string: "https://maps.googleapis.com/maps/api/directions/json?origin=\(source.latitude),\(source.longitude)&destination=\(destination.latitude),\(destination.longitude)&sensor=true&mode=driving&key=AIzaSyAneRO7LlfgaFBBH840yIdcKTL5sP866Aw")!
-//
-//            let task = session.dataTask(with: url, completionHandler: {
-//                (data, response, error) in
-//
-//                print("---> data from JSON: \(String(data: data!, encoding: .utf8))")
-//
-//                if error != nil {
-//                    print(error!.localizedDescription)
-//                   // self.activityIndicator.stopAnimating()
-//                }
-//                else {
-//                    do {
-//                        if let json : [String:Any] = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]{
-//
-//                            guard let routes = json["routes"] as? NSArray else {
-//                                DispatchQueue.main.async {
-//                                  //  self.activityIndicator.stopAnimating()
-//                                }
-//                                return
-//                            }
-//
-//                            if (routes.count > 0) {
-//                                let overview_polyline = routes[0] as? NSDictionary
-//                                let dictPolyline = overview_polyline?["overview_polyline"] as? NSDictionary
-//
-//                                let points = dictPolyline?.object(forKey: "points") as? String
-//
-//                                self.showPath(polyStr: points!)
-//
-//                                DispatchQueue.main.async {
-//                                   // self.activityIndicator.stopAnimating()
-//
-//                                    let bounds = GMSCoordinateBounds(coordinate: source, coordinate: destination)
-//                                    let update = GMSCameraUpdate.fit(bounds, with: UIEdgeInsets(top: 170, left: 30, bottom: 30, right: 30))
-//                                    self.gMapView!.moveCamera(update)
-//                                }
-//                            }
-//                            else {
-//                                DispatchQueue.main.async {
-//                                   // self.activityIndicator.stopAnimating()
-//                                }
-//                            }
-//                        }
-//                    }
-//                    catch {
-//                        print("error in JSONSerialization")
-//                        DispatchQueue.main.async {
-//                           // self.activityIndicator.stopAnimating()
-//                        }
-//                    }
-//                }
-//            })
-//            task.resume()
-//        }
-
-//        func showPath(polyStr :String){
-//            let path = GMSPath(fromEncodedPath: polyStr)
-//            let polyline = GMSPolyline(path: path)
-//            polyline.strokeWidth = 3.0
-//            polyline.strokeColor = UIColor.red
-//            polyline.map = gMapView // Your map view
-//        }
-    
+   
     
     @IBAction func dirBtn(_ sender: Any) {
         print("gsajsj \(temporaryAddress.latitude), usahgdui\(temporaryAddress.longitude)")
-        
-//        let sourceCoordinate = CLLocationCoordinate2D(latitude: 51.5072, longitude: 0.1276)
-//        // Replace temporaryAddress with the destination coordinate you want to use
-//        let destinationCoordinate = CLLocationCoordinate2D(latitude: temporaryAddress.latitude, longitude: temporaryAddress.longitude)
-//
-//        getPolylineRoute(from: sourceCoordinate, to: destinationCoordinate)
+
     }
     
     
